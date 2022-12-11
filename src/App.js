4ab4c04ca18a73198c7b4587e0fe20f821aeff1e
Zachary Duncan <zduncan27@gmail.com>
2022-12-11 18:15:15 -0500

    Change colors, add links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+library.add(faGithub, faLinkedin);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Zach Duncan</h1>
+        <div className="link-wrapper">
+          <a href="https://github.com/xzachtly" target="_blank">
+            <FontAwesomeIcon icon="fa-brands fa-github" />
+          </a>
+          <a href="https://www.linkedin.com/in/duncanz/" target="_blank">
+            <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+          </a>
+          <a href="">
+            <FontAwesomeIcon icon="fa-brands fa-instagram" />
+          </a>
+        </div>
       </header>
     </div>
   );
